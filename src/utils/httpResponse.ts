f317753cdcbbf0cf82ca72e8f7b19438f3abf76b
Adelino Penedo <agpene@hotmail.com>
2024-08-30 23:19:16 -0300

    Corrects error type on http boilerplates

--- src/utils/httpResponse.ts
+++ src/utils/httpResponse.ts
@@ -2,9 +2,14 @@
 import type { Response } from "express";
 import { LogError } from "@/utils/logger";
 
-export type ErrorHttpTypes = Error;
+export type ErrorHttpTypes = HttpError | Error;
 
-export function BadRequest(res: Response, error: Error): void {
+export interface HttpError {
+  error_code: string;
+  error_description: string;
+}
+
+export function BadRequest(res: Response, error: ErrorHttpTypes): void {
   res.status(400).json(error);
 }
 
@@ -16,28 +21,28 @@ export function Created(res: Response, data: object = {}): void {
   res.status(201).json({ ...data, success: true });
 }
 
-export function NotFound(res: Response, error: Error): void {
+export function NotFound(res: Response, error: ErrorHttpTypes): void {
   res.status(404).json(error);
 }
 
-export function InternalServerError(res: Response, error: Error): void {
+export function InternalServerError(res: Response, error: ErrorHttpTypes): void {
   if (error instanceof Error) LogError("Exceção em rotas: " + error, "InternalServerError");
   res.status(500).json(error);
 }
 
-export function UnprocessableEntity(res: Response, error: Error): void {
+export function UnprocessableEntity(res: Response, error: ErrorHttpTypes): void {
   res.status(422).json(error);
 }
 
-export function Unauthorized(res: Response, error: Error): void {
+export function Unauthorized(res: Response, error: ErrorHttpTypes): void {
   res.status(401).json(error);
 }
 
-export function Forbidden(res: Response, error: Error): void {
+export function Forbidden(res: Response, error: ErrorHttpTypes): void {
   res.status(403).json(error);
 }
 
-export function Conflict(res: Response, error: Error): void {
+export function Conflict(res: Response, error: ErrorHttpTypes): void {
   res.status(409).json(error);
 }
 
