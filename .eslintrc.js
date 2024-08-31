33041697fb615e6d0d03ff92cdda4d5f76bee791
Adelino Penedo <agpene@hotmail.com>
2024-08-30 21:15:08 -0300

    Turned off warning for camelcase on eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,56 @@
 module.exports = {
-    env: {
-      es2021: true,
-      node: true,
-    },
-    extends: [
-      "standard",
-      "plugin:@typescript-eslint/recommended",
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "standard",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-extra-semi": "off",
+    "@typescript-eslint/member-delimiter-style": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "_",
+      },
     ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: "module",
-    },
-    plugins: ["@typescript-eslint"],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/explicit-module-boundary-types": "off",
-      "@typescript-eslint/no-extra-semi": "off",
-      "@typescript-eslint/member-delimiter-style": "off",
-      "@typescript-eslint/no-var-requires": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          argsIgnorePattern: "_",
-        },
-      ],
-      "@typescript-eslint/semi": ["error"],
-      semi: "off",
-  
-      quotes: ["error", "double", { avoidEscape: true }],
-      "prefer-const": "error",
-      "space-before-function-paren": [
-        "error", {
-          anonymous: "always",
-          named: "never",
-          asyncArrow: "always",
-        },
-      ],
-      "node/no-callback-literal": "off",
-      curly: ["off", "multi"],
-      "array-bracket-newline": ["error", { multiline: true }],
-      "brace-style": ["error", "stroustrup", { allowSingleLine: true }],
-      "no-multiple-empty-lines": "off",
-  
-      "require-await": "error",
-      "no-mixed-operators": "off",
-  
-      eqeqeq: ["off", "always"],
-      "comma-dangle": ["error", "always-multiline"],
-      "no-unused-expressions": "error",
-  
-      "spaced-comment": ["error", "never"],
-    },
-  };
-  
+    "@typescript-eslint/semi": ["error"],
+    semi: "off",
+
+    quotes: ["error", "double", { avoidEscape: true }],
+    "prefer-const": "error",
+    "space-before-function-paren": [
+      "error", {
+        anonymous: "always",
+        named: "never",
+        asyncArrow: "always",
+      },
+    ],
+    "node/no-callback-literal": "off",
+    curly: ["off", "multi"],
+    "array-bracket-newline": ["error", { multiline: true }],
+    "brace-style": ["error", "stroustrup", { allowSingleLine: true }],
+    "no-multiple-empty-lines": "off",
+
+    "require-await": "error",
+    "no-mixed-operators": "off",
+
+    eqeqeq: ["off", "always"],
+    "comma-dangle": ["error", "always-multiline"],
+    "no-unused-expressions": "error",
+
+    "spaced-comment": ["error", "never"],
+  },
+};
